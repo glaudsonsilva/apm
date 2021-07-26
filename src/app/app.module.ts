51b7193428457773c2d16f3886464d0aed702eca
Glaudson Silva <glaudsonsilva@gmail.com>
2021-07-26 14:29:52 -0300

    data biding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,5 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
@@ -6,10 +7,12 @@ import { ProductListComponent } from './products/product-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent, ProductListComponent
+    AppComponent,
+     ProductListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    FormsModule
   ],
   bootstrap: [AppComponent]
 })
